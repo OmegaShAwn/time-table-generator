1bce6a5abe814da4bd134a88eaf99b3ad0ff6683
Shawn S. Thottan <shawn.thottan@freshtohome.com>
2020-02-05 02:41:39 +0530

    class wise time table generated into csv

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    ttFolder:   'timetables'
+    ttFolder: 'timetables',
+    generatedTTFolder: './generated',
 }
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const CONFIG = require('./config');
-
-//requiring path and fs modules
 const path = require('path');
 const fs = require('fs');
 
+// variables to record the timetables into.
 const timetable = {};
+let days = [];
+const times = [];
 
+// records the files into the variables.
 const readFilePromise = (file) => {
     const filename = file.split(' ').reverse()[0];
     const subject = filename.substring(0, filename.length - 4);
@@ -13,13 +15,23 @@ const readFilePromise = (file) => {
     return new Promise((resolve, reject) => {
         fs.readFile(file, 'utf8', (err, contents) => {
             if (err) reject();
-            
+
             const lines = contents.split('\r\n');
-            let days = lines.shift().split(',');
-            days.shift();
+            
+            // prevents fetching the date each iteration.
+            if (days.length === 0) {
+                days = lines.shift().split(',');
+                days.shift();
+            } else lines.shift();
+
             lines.forEach(line => {
                 const words = line.split(',');
                 const time = words.shift();
+
+                // adds the class timings into times.
+                if (!times.includes(time)) times.push(time);
+
+                // records the timetable into the timetable variable.
                 for (let j = 0; j < words.length; j++) {
                     const std = words[j].trim();
                     if (std === '') continue;
@@ -33,20 +45,47 @@ const readFilePromise = (file) => {
     });
 }
 
-//joining path of directory 
-const directoryPath = path.join(__dirname, CONFIG.ttFolder);
+const generateClassWiseTT = () => {
+    // creates folder to write into
+    if (!fs.existsSync(CONFIG.generatedTTFolder)){
+        fs.mkdirSync(CONFIG.generatedTTFolder);
+    }
 
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
+    // writes the class wise timeTables
+    Object.keys(timetable).forEach(std => {
 
-    //handling error
-    if (err) return console.log('Unable to scan directory: ' + err);
+        // creates the csv
+        let data = '--,' + days.join(',') + '\r\n';
+        data += times.map(time => {
+            return time + ',' + days.map(day => timetable[std][day][time] || '').join(',');    
+        }).join('\r\n');
+
+        // writes into the file
+        fs.writeFile(`./${CONFIG.generatedTTFolder}/${std}.csv`, data, (err) => {
+            if (err) console.log(err)
+        });
+    });
+}
+
+const compute = async() => {
+    console.log('Reading the existing time tables...');
+
+    //joining path of directory 
+    const directoryPath = path.join(__dirname, CONFIG.ttFolder);
+
+    //passsing directoryPath and callback function
+    const files = fs.readdirSync(directoryPath)
 
     const promises = [];
     
     //listing all files using forEach
     files.forEach(file => promises.push(readFilePromise(file)));
-    Promise.all(promises).then(() => {
-        console.log(timetable)
-    });
-});
+
+    await Promise.all(promises);
+
+    console.log('Generating the class wise time tables...');
+
+    generateClassWiseTT();
+}
+
+compute();
