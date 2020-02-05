355989909c2b32641106217bc414bf2f1f89ab0a
Shawn S. Thottan <shawn.thottan@freshtohome.com>
2020-02-05 14:23:07 +0530

    generates csvs for having all teachers occupied

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const fs = require('fs');
 
 // variables to record the timetables into.
 const timetable = {};
+const teachersTT = {};
 let days = [];
 const times = [];
+const teachers = [];
+const classes = [];
 
 // records the files into the variables.
-const readFilePromise = (file) => {
+const recordTimeTable = (file) => {
     const filename = file.split(' ').reverse()[0];
-    const subject = filename.substring(0, filename.length - 4);
+    const teacher = filename.substring(0, filename.length - 4);
+
+    teachers.push(teacher);
+
     file = __dirname + '\\' + CONFIG.ttFolder + '\\' + file;
     return new Promise((resolve, reject) => {
         fs.readFile(file, 'utf8', (err, contents) => {
@@ -35,9 +41,19 @@ const readFilePromise = (file) => {
                 for (let j = 0; j < words.length; j++) {
                     const std = words[j].trim();
                     if (std === '') continue;
+                    
+                    // records the classes.
+                    if (!classes.includes(std)) classes.push(std);
+                    
+                    // records the class wise timetable
                     if (!timetable[std]) timetable[std] = {};
                     if (!timetable[std][days[j]]) timetable[std][days[j]] = {};
-                    timetable[std][days[j]][time] = subject;
+                    timetable[std][days[j]][time] = teacher;
+                    
+                    // records the teachers timetable.
+                    if (!teachersTT[teacher]) teachersTT[teacher] = {};
+                    if (!teachersTT[teacher][days[j]]) teachersTT[teacher][days[j]] = {};
+                    teachersTT[teacher][days[j]][time] = std;
                 }
             })
             resolve(true);
@@ -46,13 +62,14 @@ const readFilePromise = (file) => {
 }
 
 const generateClassWiseTT = () => {
-    // creates folder to write into
-    if (!fs.existsSync(CONFIG.generatedTTFolder)){
+    // creates folder for question 1.
+    if (!fs.existsSync(CONFIG.generatedTTFolder + '/' + 1)){
         fs.mkdirSync(CONFIG.generatedTTFolder);
+        fs.mkdirSync(CONFIG.generatedTTFolder + '/' + 1);
     }
 
     // writes the class wise timeTables
-    Object.keys(timetable).forEach(std => {
+    classes.forEach(std => {
 
         // creates the csv
         let data = '--,' + days.join(',') + '\r\n';
@@ -61,14 +78,111 @@ const generateClassWiseTT = () => {
         }).join('\r\n');
 
         // writes into the file
-        fs.writeFile(`./${CONFIG.generatedTTFolder}/${std}.csv`, data, (err) => {
+        fs.writeFile(`${CONFIG.generatedTTFolder}/1/${std}.csv`, data, (err) => {
             if (err) console.log(err)
         });
     });
 }
 
+//class wise co-teachers time table
+const coTeachersCWTT = {};
+const generateTTWithMaxUtilOfTeachers = () => {
+
+    // assigns free teachers to free classes.
+    classes.forEach(std => {
+        days.forEach(day => {
+            times.forEach(time => {
+                const teacher = timetable[std][day][time];
+                if (!teacher) {
+                    teachers.find(t => {
+                        if (!teachersTT[t][day][time]) {
+                            timetable[std][day][time] = t;
+                            teachersTT[t][day][time] = std;
+                            return true;
+                        }
+                        return false;
+                    })
+                }
+            })
+        })
+    })
+
+    // assigns free teachers as co teachers.
+    classes.forEach(std => {
+        days.forEach(day => {
+            times.forEach(time => {
+                const teacher = timetable[std][day][time];
+                if (!coTeachersCWTT[std]) coTeachersCWTT[std] = {};
+                if (!coTeachersCWTT[std][day]) coTeachersCWTT[std][day] = {};
+                const coTeacher = coTeachersCWTT[std][day][time];
+                if (!coTeacher) {
+                    teachers.find(t => {
+                        if (t === teacher) return false;
+                        if (!teachersTT[t][day][time]) {
+                            coTeachersCWTT[std][day][time] = t;
+                            teachersTT[t][day][time] = std;
+                            return true;
+                        }
+                        return false;
+                    })
+                }
+            })
+        })
+    })
+
+    // creates folder for question 2.
+    if (!fs.existsSync(CONFIG.generatedTTFolder + '/' + 2)){
+        fs.mkdirSync(CONFIG.generatedTTFolder + '/' + 2);
+    }
+
+    // writes the class wise timeTables
+    classes.forEach(std => {
+
+        // creates the csv
+        let data = '--,' + days.join(',') + '\r\n';
+        data += times.map(time => {
+            return time + ',' + days.map(day => timetable[std][day][time] || '').join(',');    
+        }).join('\r\n');
+
+        // writes into the file
+        fs.writeFile(`./${CONFIG.generatedTTFolder}/2/new-${std}.csv`, data, (err) => {
+            if (err) console.log(err)
+        });
+
+        // creates the csv for the co-teachers.
+        data = '--,' + days.join(',') + '\r\n';
+        data += times.map(time => {
+            return time + ',' + days.map(day => coTeachersCWTT[std][day][time] || '').join(',');    
+        }).join('\r\n');
+
+        // writes into the file
+        fs.writeFile(`./${CONFIG.generatedTTFolder}/2/co-${std}.csv`, data, (err) => {
+            if (err) console.log(err)
+        });
+    });
+
+    // writes the teachers new time tables
+    teachers.forEach(teacher => {
+
+        // creates the csv
+        let data = '--,' + days.join(',') + '\r\n';
+        data += times.map(time => {
+            return time + ',' + days.map(day => teachersTT[teacher][day][time] || '').join(',');    
+        }).join('\r\n');
+
+        // writes into the file
+        fs.writeFile(`./${CONFIG.generatedTTFolder}/2/new-${teacher}.csv`, data, (err) => {
+            if (err) console.log(err)
+        });
+    });
+}
+
+const calculateMinimumExtraCoTeachers = () => {
+    return 0;
+}
+
 const compute = async() => {
-    console.log('Reading the existing time tables...');
+    console.log('Reading the existing time tables...\n');
 
     //joining path of directory 
     const directoryPath = path.join(__dirname, CONFIG.ttFolder);
@@ -79,13 +193,21 @@ const compute = async() => {
     const promises = [];
     
     //listing all files using forEach
-    files.forEach(file => promises.push(readFilePromise(file)));
+    files.forEach(file => promises.push(recordTimeTable(file)));
 
     await Promise.all(promises);
 
-    console.log('Generating the class wise time tables...');
+    console.log('Generating the class wise time tables...\n');
 
     generateClassWiseTT();
+
+    console.log('Generating Time table where all teachers are utilized maximum...\n');
+
+    generateTTWithMaxUtilOfTeachers();
+
+    console.log('Calculating the minimum extra co Teachers required...\n');
+
+    const count = calculateMinimumExtraCoTeachers();
 }
 
 compute();
